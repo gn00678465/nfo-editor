580492844a75fc93e3d0a5eaf60b1c12db7948fe
Madao <gn00678465@gmail.com>
2026-02-18 05:07:21 +0800

    chore: ignore electron build artifacts
    - Remove dist-electron/ from git tracking
    - Add dist-electron/ to .gitignore to prevent committing build outputs

--- dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge as n, ipcRenderer as i } from "electron";
-n.exposeInMainWorld("electronAPI", {
-  openFolder: () => i.invoke("dialog:openFolder"),
-  scanNfoFiles: (e) => i.invoke("fs:scanNfoFiles", e),
-  readFile: (e) => i.invoke("fs:readFile", e),
-  writeFile: (e, o) => i.invoke("fs:writeFile", e, o)
-});
